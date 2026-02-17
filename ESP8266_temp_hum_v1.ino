70db8acbc343e58eb20a6faade453eaace264ef4
kaiawada <77767327+kaiawada@users.noreply.github.com>
2026-02-17 11:48:33 +0200

    Add files via upload

---
+++ ESP8266_temp_hum_v1.ino
@@ -0,0 +1,78 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+#include <ArduinoJson.h>
+
+#define DHTPIN 2     
+#define DHTTYPE DHT22
+
+const char* ssid = "";
+const char* password = "";
+const char* mqtt_server = ""; 
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup_wifi() {
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  delay(5000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  
+  StaticJsonDocument<200> doc;
+  doc["temp"] = t;
+  doc["humi"] = h;
+
+  char buffer[256];
+  serializeJson(doc, buffer);
+  
+  Serial.print("Publish message: ");
+  Serial.println(buffer);
+  client.publish("Kyberno/sensor/data", buffer);
+}
